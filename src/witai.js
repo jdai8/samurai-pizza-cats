be100994a0b8dda38f236a4811501db750cae390
AyeAndy <msoohyun88@gmail.com>
2016-08-09 01:23:22 -0400

    wrote some functionalities to witai.js such as retrieving the gender and age. We can add more variables later as we expand the search options.

--- src/witai.js
+++ src/witai.js
@@ -1,9 +1,41 @@
+//required dependencies and key
 const Wit = require('../node_modules/node-wit/lib/wit.js').Wit;
 const client = new Wit({accessToken: 'S6CR2CDZVXD7W7RIRSSCLCCMWQLPCMC4'});
 
-client.message('give me people who are males', {})
-.then((data) => {
-  console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
-})
+//the user's command, !subject to change -> get it from front-end!
+var command = 'give me people who are older than 26';
 
-.catch(console.error);
+/*Some possible entities within the JSON.
+we need to predefine these variables as nulls, because they will possibly be changed later.*/
+var gender = null;
+var genderConfidence = null;
+var genderValue = null;
+var ageConfidence = null;
+var ageValue = null;
+var number = null;
+
+//contacting Wit AI and retrieving the JSON format of response
+client.message(command, {}).then((data) => {
+
+  //predicate will always almost be present.
+  var predicateConfidence = data.entities.predicate[0].confidence;
+  var predicateValue = data.entities.predicate[0].value;
+
+  //print out the predicates for testing
+  console.log('predicates: ' + predicateConfidence + " " + predicateValue);
+
+  //if there is gender entity, get its confidence and value.
+  if(typeof data.entities.gender != 'undefined'){
+  	genderConfidence = data.entities.gender[0].confidence;
+  	genderValue = data.entities.gender[0].value;
+  	console.log("gender: " + genderConfidence + " " + genderValue);
+  }
+
+  //if there is age entity, get its confidence and value.
+  if(typeof data.entities.number != 'undefined'){
+  	ageConfidence = data.entities.number[0].confidence;
+  	ageValue = data.entities.number[0].value;
+  	console.log("age: " + ageConfidence + " " + ageValue);
+  }
+
+}).catch(console.error);
